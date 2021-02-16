4fdcd5fa5db4067ea5513983d64c1ced94fe59a8
jan-leila <jan-leila@protonmai.com>
2021-02-16 04:38:04 -0600

    fixed running all functions on start

--- src/cubiomes.js
+++ src/cubiomes.js
@@ -1,15 +1,15 @@
 const cubiomes = require('../build/Release/cubiomes');
 
 module.exports = {
-	createGenerator: cubiomes.createGenerator(),
-	getStructurePos: cubiomes.getStructurePos(),
-	next: cubiomes.next(),
-	nextInt: cubiomes.nextInt(),
-	nextLong: cubiomes.nextLong(),
-	nextFloat: cubiomes.nextFloat(),
-	nextDouble: cubiomes.nextDouble(),
-	applySeed: cubiomes.applySeed(),
-	isViableStructurePos: cubiomes.isViableStructurePos(),
-	getBiomeAtPos: cubiomes.getBiomeAtPos(),
-	isOceanic: cubiomes.isOceanic(),
+	createGenerator: cubiomes.createGenerator,
+	getStructurePos: cubiomes.getStructurePos,
+	next: cubiomes.next,
+	nextInt: cubiomes.nextInt,
+	nextLong: cubiomes.nextLong,
+	nextFloat: cubiomes.nextFloat,
+	nextDouble: cubiomes.nextDouble,
+	applySeed: cubiomes.applySeed,
+	isViableStructurePos: cubiomes.isViableStructurePos,
+	getBiomeAtPos: cubiomes.getBiomeAtPos,
+	isOceanic: cubiomes.isOceanic,
 }
